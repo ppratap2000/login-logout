3b5e9e01413d234d153f8681476a67829623e9d3
ppratap2000 <pranavstudy2022@gmail.com>
2023-05-18 17:49:46 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+  };
+
+  const handleGenerateOTP = () => {
+    // Make an API call to the server to generate OTP and send it via SMS
+    fetch("http://localhost:3000/generate-otp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ phone }),
+    })
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const handleVerifyOTP = () => {
+    // Make an API call to the server to verify the entered OTP
+    fetch("http://localhost:3000/verify-otp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ phone, otp }),
+    })
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login/Logout with OTP</h1>
+      <label>
+        Phone:
+        <input type="text" value={phone} onChange={handlePhoneChange} />
+      </label>
+      <br />
+      <button onClick={handleGenerateOTP}>Generate OTP</button>
+      <br />
+      <label>
+        OTP:
+        <input type="text" value={otp} onChange={handleOtpChange} />
+      </label>
+      <br />
+      <button onClick={handleVerifyOTP}>Verify OTP</button>
+      <br />
+      <p>{message}</p>
     </div>
   );
 }
